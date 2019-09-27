5a61d42f90d3da8c35f5b0f877e645bbfe1edf48
MAITRE Nicolas <Nicolas.MAITRE@cpnv.ch>
2019-09-27 15:03:37 +0200

    handles + hours display + fps counter

--- boot.js
+++ boot.js
@@ -24,23 +24,40 @@ Number.prototype.fillGap = String.prototype.fillGap;
 //BOOT
 //build
 var testDisplay = document.body.addElement("div", "saucisse");
+document.body.addElement("div", "saucisseSeparator");
+var testDisplayMin = document.body.addElement("div", "saucisseMin");
+document.body.addElement("div", "saucisseSeparator");
+var testDisplaySec = document.body.addElement("div", "saucisseSec");
+document.body.addElement("div", "saucisseSeparator");
+var testDisplayDec = document.body.addElement("div", "saucisseDec");
+document.body.addElement();
 var clockContainer = document.body.addElement("div", "clockContainer");
 var clockCenter = clockContainer.addElement("div", "clockCenter");
+var clockTopLine = clockContainer.addElement("div", "clockLine");
+var fpsCounter = document.body.addElement("div", "fpsCounter");
 //handles
 var handles = {};
-handles.hours = {elem: clockContainer.addElement("div", "clockHandleContainer hour")};
+handles.hours = {elem: clockContainer.addElement("div", "clockHandleContainer hour"), turns: 0, lastDeg:0};
 handles.hours.elem.addElement("div", "clockHandle");
-handles.minutes = {elem: clockContainer.addElement("div", "clockHandleContainer min")};
-handles.minutes.elem.addElement("div", "clockHandle");
-handles.seconds = {elem: clockContainer.addElement("div", "clockHandleContainer sec")};
+handles.minutes = {elem: clockContainer.addElement("div", "clockHandleContainer min"), turns: 0, lastDeg:0};
+handles.minutes.handleElem = handles.minutes.elem.addElement("div", "clockHandle");
+handles.seconds = {elem: clockContainer.addElement("div", "clockHandleContainer sec"), turns: 0, lastDeg:0};
 handles.seconds.elem.addElement("div", "clockHandle");
-handles.dec = {elem: clockContainer.addElement("div", "clockHandleContainer dec")};
-handles.dec.elem.addElement("div", "clockHandle");
+handles.dec = {elem: clockContainer.addElement("div", "clockHandleContainer dec"), turns: 0, lastDeg:0};
+handles.dec.handleElem = handles.dec.elem.addElement("div", "clockHandle");
 /*var decHandle = clockContainer.addElement("div", "clockHandleContainer dec");
 decHandle.addElement("div", "clockHandle");*/
 
+fetch('http://intranet.cpnv.ch/classes/SI-T1a/horaire?fullcalendar=true&start=1569189600&end=1569708000').then(function(res){
+    console.log(res);
+    return res.text();
+}).then(function(json){
+    console.log(json);
+});
+
 againAndAngain();
-function againAndAngain(){
+var frameCount = 0;
+function againAndAngain(newFrameCount){
     //date
     var currentDate = new Date();
     var hours = currentDate.getHours();
@@ -48,16 +65,38 @@ function againAndAngain(){
     var seconds = currentDate.getSeconds();
     var milliseconds = currentDate.getMilliseconds();
     //numeric
-    testDisplay.innerText = hours.fillGap() + " : " + minutes.fillGap() + " : " + seconds.fillGap() + " : " + milliseconds.fillGap(3);
+    testDisplay.innerText = hours.fillGap();
+    testDisplayMin.innerText = minutes.fillGap();
+    testDisplaySec.innerText = seconds.fillGap();
+    testDisplayDec.innerText = milliseconds.fillGap(3);
     //handles
     handleRotate(handles.hours, (hours/24)*360);
     handleRotate(handles.minutes, (minutes/60)*360);
     handleRotate(handles.seconds, (seconds/60)*360);
-    handleRotate(handles.dec, (milliseconds/1000)*360);
+    handleRotate(handles.dec, (milliseconds/1000)*360, true);
     //decHandle.style.transform = "rotate(" + (milliseconds/1000)*360 + "deg)";
+
+    //shitty colors
+    var multicolorstring = "rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")";
+    handles.dec.handleElem.style["border-color"] = multicolorstring;
+    testDisplayDec.style["color"] = multicolorstring;
+
+    //dynamic colors
+    var minColor = "rgb(0," + (255 - (seconds/60*255)) + ", 0)";
+    testDisplayMin.style.color = minColor;
+    handles.minutes.handleElem.style["border-color"] = minColor;
+
+    var frameTime = newFrameCount - frameCount;
+    frameCount = newFrameCount;
+    var fps = Math.floor(1000/frameTime);
+    fpsCounter.innerText = fps + " fps";
     //callback
     requestAnimationFrame(againAndAngain);
 }
-function handleRotate(handle,deg){
-    handle.elem.style.transform = "rotate(" + deg + "deg)";
+function handleRotate(handle,deg,disableCheck=false){
+    if(handle.lastDeg > deg && !disableCheck){
+        handle.turns++;
+    }
+    handle.lastDeg = deg;
+    handle.elem.style.transform = "rotate(" + (handle.turns*360 + deg) + "deg)";
 }
